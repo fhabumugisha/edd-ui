11c5e752c719b20a15577749d3e0d2f08a8ab1bf
fahabumu <fahabumu@LFR019188.corp.capgemini.com>
2017-10-09 21:49:22 +0200

    add crud operations

--- edd-ui/src/app/theme/theme.component.ts
+++ edd-ui/src/app/theme/theme.component.ts
@@ -10,10 +10,29 @@ import {Theme} from './theme';
 export class ThemeComponent implements OnInit {
 
   themes: Theme[];
+  newTheme: Theme;
+  selectedTheme: Theme;
   constructor(private router: Router, private themeService: ThemeService) { }
 
   ngOnInit() {
     this.themeService.getThemes().then(themes => this.themes = themes);
+    this.newTheme = new Theme();
+  }
+
+  createTheme (theme: Theme): void {
+    this.themeService.createTheme(theme).then( theme => {
+    this.themes.push(theme);
+      this.selectedTheme = null;
+    });
+    }
+
+  deleteTheme(theme: Theme): void {
+    this.themeService.deleteTheme(theme).then(() => {
+      this.themes = this.themes.filter(h => h !== theme);
+      if ( this.selectedTheme === theme) {
+        this.selectedTheme = null;
+      }
+    });
   }
 
 }
--- edd-ui/src/app/theme/theme.service.ts
+++ edd-ui/src/app/theme/theme.service.ts
@@ -10,12 +10,32 @@ export class ThemeService {
 
   constructor(private http: Http) { }
 
+  private headers = new Headers({ 'Content-Type' : 'application/json'});
 
   getThemes(): Promise<Theme[]> {
     return this.http.get(this.themeUrl).toPromise().then(response => response.json() as Theme[]).catch(this.handleError);
 
   }
 
+  getTheme(id: number): Promise<Theme> {
+    const url = `${this.themeUrl}/getTheme/${id}`;
+    return this.http.get(url).toPromise().then(response => response.json() as Theme).catch(this.handleError);
+  }
+
+  createTheme(theme: Theme): Promise<Theme> {
+    return this.http.post(this.themeUrl + '/createTheme', JSON.stringify(theme), {headers : this.headers}).toPromise()
+                  .then(response => response.json() as Theme).catch(this.handleError);
+  }
+
+    updateTheme(theme: Theme): Promise<Theme> {
+    return this.http.post(this.themeUrl + '/updateTheme', JSON.stringify(theme), {headers : this.headers}).toPromise()
+                  .then(() => theme).catch(this.handleError);
+  }
+
+   deleteTheme(theme: Theme): Promise<Theme> {
+    const url = `${this.themeUrl}/deleteTheme/${theme.id}`;
+    return this.http.get(url, { headers: this.headers }).toPromise().then(() => null).catch(this.handleError);
+  }
   private handleError(error: any): Promise<any> {
      console.error('An error occurred', error);
     return Promise.reject(error.message || error);
